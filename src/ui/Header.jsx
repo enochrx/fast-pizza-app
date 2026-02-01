ca37fc7537795bfd1ed8b60ba2bfe7652c59f610
enochrx <drreayy@gmail.com>
2026-02-01 23:05:45 +0100

    styling with tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Swift Pizza Co.</Link>
+    <header className="bg-rose-500 uppercase">
+      <Link className="font-extrabold tracking-[1rem]" to="/">
+        Swift Pizza Co.
+      </Link>
       <SearchOrder />
       <p>Ray Cuisine</p>
     </header>
