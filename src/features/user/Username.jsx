0d9173c261732b581da146de5c5aebd5b452bd4e
enochrx <drreayy@gmail.com>
2026-02-02 22:55:51 +0100

    responsive design

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+const Username = () => {
+  return <div className="text-sm font-semibold">Ray Sean</div>;
+};
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
     <header className="bg-rose-500 uppercase">
       <Link className="font-extrabold tracking-[1rem]" to="/">
-        Swift Pizza Co.
+        <div className="inline-flex items-center justify-between">
+          <img
+            src="../../public/pizza.png"
+            alt="pizza.png"
+            className="max-h-10 w-auto md:max-h-12"
+          />
+          Swift Pizza Co.
+        </div>
       </Link>
       <SearchOrder />
-      <p>Ray Cuisine</p>
+      <Username />
     </header>
   );
 };
