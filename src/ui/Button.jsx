831f1b3a734fd5819a8f6581ece5f1cdc4d59835
enochrx <drreayy@gmail.com>
2026-02-20 10:57:20 +0100

    positioning loader component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,18 @@
-const Button = ({ children, disabled }) => {
+import { Link } from "react-router-dom";
+
+const Button = ({ children, disabled, to }) => {
+  const className =
+    "transition-color inline-block rounded-full bg-rose-500 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase duration-300 hover:bg-rose-400 focus:ring focus:ring-rose-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      className="transition-color inline-block rounded-full bg-rose-500 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase duration-300 hover:bg-rose-400 focus:ring focus:ring-rose-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4"
-      disabled={disabled}
-    >
+    <button className={className} disabled={disabled}>
       {children}
     </button>
   );
