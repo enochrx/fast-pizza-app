d04b7ca32dac65df9eed951e61f929c96b57f056
enochrx <drreayy@gmail.com>
2026-02-25 19:28:35 +0100

    styled the form and cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -44,7 +44,7 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const Button = ({ children, disabled, to, type, opacity }) => {
   const base =
-    "transition-color inline-block rounded-full bg-rose-500  font-semibold tracking-wide text-stone-800 uppercase duration-300 hover:bg-rose-400 focus:ring focus:ring-rose-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ";
+    "transition-color text-sm inline-block rounded-full bg-rose-500  font-semibold tracking-wide text-stone-800 uppercase duration-300 hover:bg-rose-400 focus:ring focus:ring-rose-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + ` px-4 py-2 text-xs md:px-5 md:py-2.5 ${opacity}`,
+    secondary:
+      "transition-color text-sm inline-block rounded-full border-2 border-stone-400 bg-transparent font-semibold tracking-wide text-stone-500 uppercase duration-300 hover:bg-stone-300 hover:text-stone-800 focus:ring focus:text-stone-800 focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
 
   if (to)
